4a55fe09dfe46edfb4a4f31bad0d5301dd7da062
Cutelemon6 <cutelemon6@qq.com>
2025-06-12 12:42:12 +0800

    refine support version

--- paddle/phi/backends/dynload/cusolver.h
+++ paddle/phi/backends/dynload/cusolver.h
@@ -138,7 +138,7 @@ CUSOLVER_ROUTINE_EACH_R1(DECLARE_DYNAMIC_LOAD_CUSOLVER_WRAP)
 CUSOLVER_ROUTINE_EACH_R2(DECLARE_DYNAMIC_LOAD_CUSOLVER_WRAP)
 #endif
 
-#if CUDA_VERSION >= 11100
+#if CUDA_VERSION >= 11010
 #define CUSOLVER_ROUTINE_EACH_R3(__macro) \
   __macro(cusolverDnXgetrf);              \
   __macro(cusolverDnXgetrf_bufferSize);   \
